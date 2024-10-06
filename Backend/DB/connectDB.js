4349f1c07b41608fc61ba0b37822103dc4dcf8b7
developerdev2007 <160243215+developerdev2007@users.noreply.github.com>
2024-10-06 13:22:51 +0530

    vercel deployment

--- Backend/DB/connectDB.js
+++ Backend/DB/connectDB.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUniFiedTopology: true,
+      // useNewUrlParser: true,
+      // useUniFiedTopology: true,
     });
     console.log(`MongoDb Connected : ${conn.connection.host}`);
   } catch (error) {
--- frontend/src/hooks/useToastHook.js
+++ frontend/src/hooks/useToastHook.js
@@ -1,17 +1,20 @@
-import { useToast } from "@chakra-ui/toast";
+import { useToast } from "@chakra-ui/react";
 import { useCallback } from "react";
 
 const useToastHook = () => {
   const toast = useToast();
-  const showToast = useCallback((title, description, status) => {
-    toast({
-      title,
-      description,
-      status,
-      duration: 5000,
-      isClosable: true,
-    });
-  },[toast]);
+  const showToast = useCallback(
+    (title, description, status) => {
+      toast({
+        title,
+        description,
+        status,
+        duration: 5000,
+        isClosable: true,
+      });
+    },
+    [toast]
+  );
   return showToast;
 };
 
