28db0baf7d026919f8d69ebb3ee7cee64feb6f78
developerdev2007 <160243215+developerdev2007@users.noreply.github.com>
2024-10-06 13:53:42 +0530

    secure true in config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       "/api": {
         target: "https://mern-stack-thread-backend.vercel.app",
         changeOrigin: true,
-        secure: false,
+        secure: true,
       },
     },
   },
